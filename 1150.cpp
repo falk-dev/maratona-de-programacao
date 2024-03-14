763d410431969fac799252004841682fd5177f00
Mychelle Falk <mychelleketlen04@gmail.com>
2024-03-14 13:36:02 -0500

    1150

---
+++ 1150.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int X = 0;
+    int Z = 0;
+    int aux = 1;
+
+    cin >> X;
+
+    int soma = X;
+
+    while (X >= Z)
+    {
+        cin >> Z;
+    }
+
+    do
+    {
+        X++;
+        soma = soma + X;
+        aux++;
+    } while (soma < Z);
+
+    cout << aux << endl;
+}
