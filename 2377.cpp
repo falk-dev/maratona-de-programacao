2653ae8563d773307c29b5f123ace3aaf54e4410
mychellerocha <mychelleketlen04@gmail.com>
2024-03-16 19:03:25 -0300

    2377

---
+++ 2377.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int L = 0;
+    int D = 0; 
+    int K = 0;
+    int P = 0;
+    int CG = 0;
+    int CP = 0;
+    int i = 0;
+    int CT = 0;
+
+    cin >> L >> D;
+    cin >> K >> P;
+
+    CG = L * K;
+
+    if (L >= D)
+    {
+        do
+        {
+            CP = P * i;
+            i++;
+        } while (D * i <= L);
+    }
+    
+    CT = CP + CG;
+    cout << CT << endl;
+
+    return 0;
+}
