7d4eadd9ca66ede1fa7b6cb7814b9d5ff61d4560
mychellerocha <mychelleketlen04@gmail.com>
2024-03-16 16:18:13 -0300

    2339

---
+++ 2339.cpp
@@ -0,0 +1,23 @@
+// 2339 - AVIÕES DE PAPEL | RESOLVIDO EM C++
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int C = 0;
+    int P = 0;
+    int F = 0;
+
+    cin >> C >> P >> F;
+
+    if (C * F <= P) {
+        cout << "S" << endl;
+    }
+    else {
+        cout << "N" << endl;
+    }
+
+    return 0;
+}
