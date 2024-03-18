e78580af077b8bcca9f05d31a09da83816704d6c
mychellerocha <mychelleketlen04@gmail.com>
2024-03-18 02:37:32 -0300

    ‎

---
+++ 1016.cpp
@@ -0,0 +1,18 @@
+// 1016 - DISTÂNCIA | RESOLVIDO EM C++
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int N = 0;
+    int T = 0;
+
+    cin >> N;
+
+    T = N * 2;
+    cout << T << " minutos" << endl;
+
+    return 0;
+}
--- 1017.cpp
+++ 1017.cpp
@@ -1,12 +1,22 @@
 // 1017 - GASTO DE COMBUSTÍVEL | RESOLVIDO EM C++
 
 #include <iostream>
+#include <iomanip>
 
 using namespace std;
 
 int main()
 {
-    
+    int T = 0;
+    int V = 0;
+    float G = 0.0;
+
+    cin >> T;
+    cin >> V;
+
+    G = (T * V) / 12.0;
+
+    cout << fixed << setprecision(3) << G << endl;
 
     return 0;
 }
