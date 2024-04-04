bf263d894fd290e603144df0b4b25f629c9e3048
Mychelle Falk <mychelleketlen04@gmail.com>
2024-04-04 07:27:45 -0500

    .

--- 2663/main.cpp
+++ 2663/main.cpp
@@ -1,13 +1,45 @@
-// 2663 - FASE | RESOLVIDO EM C++ 
+// 2663 - FASE | RESOLVIDO EM C++
 
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
 int main()
 {
-   int N = 0;
-   int K = 0;
+    int N = 0;
+    int K = 0;
+    int i = 0;
+    int j = 0;
+    int aux = 0;
+
+    cin >> N;
+    cin >> K;
+
+    aux = K;
+
+    vector<int> X(N);
+
+    for (i = 0; i < N; i++)
+    {
+        cin >> X[i];
+    }
+
+    sort(X.begin(), X.end(), greater<int>());
+
+    for (i = 0; i < N; i++)
+    {
+        if (X[K] == X[K - 1])
+        {
+            while(X[K+j] == X[K-1]) {
+                aux++;
+                j++;
+            }
+        }
+    }
+
+    cout << aux << endl;
 
     return 0;
 }
