0102b4013c80fa25885f12d324def8e614c4358c
Mychelle Falk <mychelleketlen04@gmail.com>
2024-04-15 08:19:56 -0500

    questoes

---
+++ lpc/aula-01-introducao/D/main.cpp
@@ -0,0 +1,17 @@
+// D - PETER'S SMOKES || RESOLVIDO EM C++
+// Author: falk.dev
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n, k;
+    
+    while (cin >> n >> k) {
+        
+    }
+
+    return 0;
+}
