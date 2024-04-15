fcf678e3d1a22691ca4a0bcc83c4f03bd6498786
Mychelle Falk <mychelleketlen04@gmail.com>
2024-04-15 08:12:09 -0500

    questoes

--- lpc/aula-01-introducao/C/main.cpp
+++ lpc/aula-01-introducao/C/main.cpp
@@ -5,20 +5,26 @@
 
 using namespace std;
 
-int main() {
+int main()
+{
     int n, s, d, x1 = 0, x2 = 0;
 
     cin >> n;
 
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
+    {
         cin >> s >> d;
 
-        if (s < d) {
+        if ((s + d) % 2 == 0 && (s - d) >= 0)
+        {
+            x2 = abs((d - s) / 2);
+            x1 = s - x2;
+
+            cout << x1 << " " << x2 << endl;
+        }
+        else
+        {
             cout << "impossible" << endl;
-        } else {
-            x1 + x2 = s;
-            x2 = s - x1;
-            x1*(s - x1) = s;
         }
     }
 
