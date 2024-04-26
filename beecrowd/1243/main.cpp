ceda1f3c47dc6abda1ca28eee06200b1df59bdf7
mychellerocha <mychelleketlen04@gmail.com>
2024-04-25 19:01:13 -0500

    questoes

---
+++ beecrowd/1243/main.cpp
@@ -0,0 +1,53 @@
+// 1243 - O QUÃO FÁCIL É... | RESOLVIDO EM C++
+// Author: falk.dev
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Printar vector
+#define dbvector(vector)          \
+    {                             \
+        cout << #vector << " = "; \
+        for (auto &it : vector)   \
+            cout << it << " ";    \
+        cout << endl;             \
+    }
+
+// Fast IO
+#define SPEED                     \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);
+
+// Função de Debbug
+#define dbg(x) cout << #x << " = " << x << endl;
+
+// Nome alternativo para long long
+typedef long long ll;
+
+// Pair
+#define f first;
+#define s second;
+
+int main()
+{
+    SPEED;
+
+    string problema;
+
+    while (getline(cin, problema))
+    {
+        int aux = 0;
+        for (int i = 0; i < problema.size(); i++)
+        {
+            if (isalpha(problema[i]))
+            {
+                aux++;
+            }
+        }
+
+        cout << aux << endl;
+    }
+
+    return 0;
+}
