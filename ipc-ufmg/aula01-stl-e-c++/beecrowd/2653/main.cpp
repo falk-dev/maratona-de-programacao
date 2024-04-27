3e58a4cdc2424c86d47de915ad52202a52efb9c9
mychellerocha <mychelleketlen04@gmail.com>
2024-04-27 01:10:46 -0500

    questoes

---
+++ ipc-ufmg/aula01-stl-e-c++/beecrowd/2653/main.cpp
@@ -0,0 +1,47 @@
+// 2653 - DIJKSTRA | RESOLVIDO EM C++
+// Author: falk.dev
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Printar vector
+#define dbvector(vector)          \
+    {                             \
+        cout << #vector << " = "; \
+        for (auto &it : vector)   \
+            cout << it << " ";    \
+        cout << endl;             \
+    }
+
+// Fast IO
+#define SPEED                     \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);
+
+// Função de Debbug
+#define dbg(x) cout << #x << " = " << x << endl;
+
+// Nome alternativo para long long
+typedef long long ll;
+
+// Pair
+#define f first;
+#define s second;
+
+int main()
+{
+    SPEED;
+
+    string elementos;
+    set<string> joias;
+
+    while (cin >> elementos)
+    {
+        joias.insert(elementos);
+    }
+
+    cout << joias.size() << endl;
+
+    return 0;
+}
