90293cb2cf184c8f00b2912455f8be0743199493
Luis Santiago <luis.santiago@btcambio.com.br>
2024-07-01 21:52:25 -0500

    Same Differences

--- Codeforces/1520D. Same Differences/main.cpp
+++ Codeforces/1520D. Same Differences/main.cpp
@@ -42,37 +42,26 @@ typedef priority_queue<pii, vector<pii>, greater<pii>> pq_min;
 
 #define db_pair(x) cout << #x << ": " << x.first << ", " << x.second << endl;
 
-int main()
-{
+int main() {
     SPEED;
 
     int n_casos;
-
     cin >> n_casos;
 
-    while (n_casos--)
-    {
-        int n, aux = 0;
+    while (n_casos--) {
+        int qtd_el;
+        ll aux = 0;
+        cin >> qtd_el;
+        vi elementos(qtd_el);
+        map<int, int> ocorrencias;
 
-        cin >> n;
-
-        vector<int> nums(n);
-
-        for (int i = 0; i < n; i++)
-        {
-            cin >> nums[i];
+        for (int i = 0; i < qtd_el; i++) {
+            cin >> elementos[i];
         }
 
-        for (int i = 0; i < n; i++)
-        {
-            for (int j = i - 1; j >= 0; j--)
-            {
-                int dif = nums[j] - nums[i];
-                int ind = j - i;
-                if (dif == ind) {
-                    aux++;
-                }
-            }
+        for (int i = 0; i < qtd_el; i++) {
+            aux += ocorrencias[elementos[i] - i];
+            ocorrencias[elementos[i] - i]++;
         }
 
         cout << aux << endl;
