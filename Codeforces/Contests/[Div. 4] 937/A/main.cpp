993921dc33b95bec802341c9996bca1b1bde0581
Falk Dev <mychelleketlen04@gmail.com>
2025-04-25 19:38:33 -0500

    contests

---
+++ Codeforces/Contests/[Div. 4] 937/A/main.cpp
@@ -0,0 +1,66 @@
+/*
+ *  Link da questão:
+ *  xxxx - xxxx | RESOLVIDO EM C++
+ *  Author: falk.dev
+ *
+ */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define SPEED                     \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);
+
+#define dbg(x) cout << #x << " = " << x << endl;
+
+#define dbvector(vector)          \
+    {                             \
+        cout << #vector << " = "; \
+        for (auto &it : vector)   \
+            cout << it << " ";    \
+        cout << endl;             \
+    }
+
+#define dbmap(map)                                  \
+    for (auto e : map)                              \
+        cout << e.first << " " << e.second << endl; \
+    cout << endl;
+
+typedef long long ll;
+
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef long double ld;
+typedef priority_queue<pii, vector<pii>, greater<pii>> pq_min;
+
+#define f first;
+#define s second;
+
+#define db_pair(x) cout << #x << ": " << x.first << ", " << x.second << endl;
+
+int main() {
+    SPEED;
+
+    int casos;
+
+    cin >> casos;
+
+    while (casos--) {
+        int a, b, c;
+
+        cin >> a >> b >> c;
+
+        if (a < b && b < c)
+            cout << "STAIR" << endl;
+        else if (a < b && b > c)
+            cout << "PEAK" << endl;
+        else
+            cout << "NONE" << endl;
+    }
+
+    return 0;
+}
